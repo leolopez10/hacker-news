a34f1fbefeb92c7bd0d64e6239dfc50a2f52944f
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-24 17:44:55 -0500

    made it so the pages only loads the results after the search button is hit

--- src/App.js
+++ src/App.js
@@ -4,29 +4,35 @@ function App() {
   // state
   const [news, setNews] = useState([])
   const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
 
   //fetch news
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       // .then(data => console.log(data.hits))
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
   };
 
+  //only loads when the url changes
   useEffect(() => {
     fetchNews()
-  }, [searchQuery])
+  }, [url])
 
   const handleChange = (event) => {
     setSearchQuery(event.target.value)
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
 
   return (
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type='text' value={searchQuery} onChange={handleChange} />
         <button>SEARCH</button>
       </form>
