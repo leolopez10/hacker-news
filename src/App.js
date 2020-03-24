ae2f6a5ac9769d2daad3ac984936b5b7ab9c9af3
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-24 17:20:39 -0500

    created a list of news article titles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  // state
+  const [news, setNews] = useState([])
+
+  //fetch news
+  const fetchNews = () => {
+    fetch('http://hn.algolia.com/api/v1/search?query=react')
+    .then(result => result.json())
+    .then(data => setNews(data.hits))
+    .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchNews()
+  })
+
+  return(
+    <div>
+      <h2>News</h2>
+      {news.map((news, index) => (<p key={index}>{news.title}</p>))}
+    </div>
+  )
 }
 
 export default App;
