9c8b28c932da2ad3aca47b6381f6aa86520f9b75
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-24 17:34:13 -0500

    created a search input and button to search hacker news

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import React, { Component, useState, useEffect } from 'react';
 function App() {
   // state
   const [news, setNews] = useState([])
+  const [searchQuery, setSearchQuery] = useState('react')
 
   //fetch news
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
-    .then(result => result.json())
-    .then(data => setNews(data.hits))
-    .catch(error => console.log(error));
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+      .then(result => result.json())
+      // .then(data => console.log(data.hits))
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
   };
 
   useEffect(() => {
     fetchNews()
   })
 
-  return(
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
+
+  return (
     <div>
       <h2>News</h2>
-      {news.map((news, index) => (<p key={index}>{news.title}</p>))}
+      <form>
+        <input type='text' value={searchQuery} onChange={handleChange} />
+        <button>SEARCH</button>
+      </form>
+      {news.map((news, index) => (
+        <p key={index}>{news.title}</p>
+      ))}
     </div>
   )
 }
