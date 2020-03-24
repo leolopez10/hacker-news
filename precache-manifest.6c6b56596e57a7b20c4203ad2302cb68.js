d2b06a465ff07dc14fffb14662c7faed9a5fb933
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-24 17:04:13 -0500

    Updates

---
+++ precache-manifest.6c6b56596e57a7b20c4203ad2302cb68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103571f230c429a6bc7222951c87d7d1",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "fb21b355f2f3fc24669b",
+    "url": "/hacker-news/static/js/2.e4ecfc7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.e4ecfc7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d970171cce5b076a3cb",
+    "url": "/hacker-news/static/js/main.f8e3e727.chunk.js"
+  },
+  {
+    "revision": "3058bd9c9b16bc5cbe6d",
+    "url": "/hacker-news/static/js/runtime-main.b66f509d.js"
+  }
+]);
