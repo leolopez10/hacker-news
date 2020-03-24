2d530c40cf82408089189ea43ba0c9ca05575f59
lil L <57925562+leolopez10@users.noreply.github.com>
2020-03-24 17:56:51 -0500

    organized code into function

--- src/App.js
+++ src/App.js
@@ -31,17 +31,27 @@ function App() {
     setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
   }
 
+  const showLoading = () => (loading ? <h2>LOADING...</h2> : "")
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={searchQuery} onChange={handleChange} />
+      <button>SEARCH</button>
+    </form>
+  )
+
+  const showNews = () => {
+    return news.map((news, index) => (
+      <p key={index}>{news.title}</p>
+    ))
+  }
+
   return (
     <div>
       <h2>News</h2>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value={searchQuery} onChange={handleChange} />
-        <button>SEARCH</button>
-      </form>
-      {loading ? <h2>LOADING...</h2> : ""}
-      {news.map((news, index) => (
-        <p key={index}>{news.title}</p>
-      ))}
+      {searchForm()}
+      {showLoading()}
+      {showNews()}
     </div>
   )
 }
